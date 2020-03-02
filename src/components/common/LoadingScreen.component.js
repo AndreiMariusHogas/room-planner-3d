9ecd56542eb7912c6b2737359bf6e63d324996ce
Hogas Andrei Marius <hogasandreimarius@gmail.com>
2020-03-02 16:52:55 +0100

    Added Loading Screen

--- src/components/common/LoadingScreen.component.js
+++ src/components/common/LoadingScreen.component.js
@@ -1,13 +1,19 @@
-import React, { PureComponent } from 'react'
+// React
+import React from 'react'
+// Materail UI
+import withStyles from '@material-ui/core/styles/withStyles'
+import LinearProgress from '@material-ui/core/LinearProgress'
+import Typography from '@material-ui/core/Typography'
+import globalStyles from '../../styles/globalStyles'
 
-export class LoadingScreen extends PureComponent {
-  render() {
-    return (
-      <div>
-        This is the loading screen
-      </div>
-    )
-  }
+export const LoadingScreen = (props) => {
+  const { classes } = props
+  return (
+    <>
+      <Typography variant='h4' className={classes.loadingText}>Loading...</Typography>
+      <LinearProgress className={classes.loading} color='primary' />
+    </>
+  )
 }
 
-export default LoadingScreen
+export default withStyles(globalStyles)(LoadingScreen)
