d89a83e4ff6c6a8d498844f76edf71dacaa2a2d5
Hogas Andrei Marius <hogasandreimarius@gmail.com>
2020-03-03 05:13:19 +0100

    Added Loader

--- src/components/common/LoadingScreen.component.js
+++ src/components/common/LoadingScreen.component.js
@@ -1,25 +1,17 @@
 // React
 import React from 'react'
-import PropTypes from 'prop-types'
-// Materail UI
-import withStyles from '@material-ui/core/styles/withStyles'
-import LinearProgress from '@material-ui/core/LinearProgress'
-import Typography from '@material-ui/core/Typography'
-// Styles
-import styles from '../../styles/styles'
 
 export const LoadingScreen = (props) => {
-  const { classes } = props
   return (
-    <div>
-      <Typography variant='h4' className={classes.loadingText}>Loading...</Typography>
-      <LinearProgress className={classes.loading} color='primary' />
+    <div id='loader'>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
     </div>
   )
 }
 
-LoadingScreen.propTypes = {
-  classes: PropTypes.object.isRequired
-}
-
-export default withStyles(styles)(LoadingScreen)
+export default (LoadingScreen)
